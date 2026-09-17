f34c827d7f45317d47b207b563f659c121414715
Zoya Khan <zoyakhan@example.com>
2026-09-17 22:50:51 +0530

    Strings.py

---
+++ Strings.py
@@ -0,0 +1,12 @@
+# Stings are immutable thay cannt change
+nm="Harry"
+print(nm[-4:-2])
+
+# methods 
+name="Zoya"
+print(len(name))
+print(name.upper())
+print(name.lower())
+print(name.rstrip("!"))
+print(name.replace('Zoya','Aalima'))
+print(name.count("Zoya"))
