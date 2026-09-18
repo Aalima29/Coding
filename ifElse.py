9b92fdc7d47a006fb22359140054fea77d6891b2
Zoya Khan <zoyakhan@example.com>
2026-09-18 23:29:31 +0530

    loops.py

--- ifElse.py
+++ ifElse.py
@@ -1,32 +1,32 @@
-# a=int(input("Enter your age: "))
-# if (a>18):
-#     print("You are eligible to vote")
-# else:
-#     print("You are not eligible for vote")
+a=int(input("Enter your age: "))
+if (a>18):
+    print("You are eligible to vote")
+else:
+    print("You are not eligible for vote")
 
-# b=int (input("Enter any number : "))
-# if(b<0):
-#     print("num is negative")
-# elif(b==0):
-#     print("num is equal to zero")
-# else:
-#     print("num is positive")
+b=int (input("Enter any number : "))
+if(b<0):
+    print("num is negative")
+elif(b==0):
+    print("num is equal to zero")
+else:
+    print("num is positive")
 
 
-# # Nested 
-# age=int(input("Enter your age: "))
-# if(age==0):
-#     print("New Born")
-# elif(age>0):
-#     if(age<=10):
-#         print("Infant to toddler")
-#     elif(age>10 and age<20):
-#         print("You are a teenager")
-#     elif(age>20 and age<40):
-#         print("You are adult")
+# Nested 
+age=int(input("Enter your age: "))
+if(age==0):
+    print("New Born")
+elif(age>0):
+    if(age<=10):
+        print("Infant to toddler")
+    elif(age>10 and age<20):
+        print("You are a teenager")
+    elif(age>20 and age<40):
+        print("You are adult")
 
-# else:
-#     print("you are older")
+else:
+    print("you are older")
 
 
 # Program to greet acc to time
---
+++ loops.py
@@ -0,0 +1,17 @@
+# num=int(input("Enter any number to see the table: "))
+# for i in range(2,11):
+#     print(num ,"X " , i ,"= ", num*i)
+
+num=int(input("Enter any number : "))
+i=1
+while(i<=10):
+    result=num*i
+    print(result)
+    i= i+1
+    
+   
+
+    
+     
+    
+    
