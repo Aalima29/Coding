b8f1c4638f8421a7ef98f593267bb1505c5c7a1d
Zoya Khan <zoyakhan@example.com>
2026-09-18 12:42:23 +0530

    ifElse.py

--- ifElse.py
+++ ifElse.py
@@ -1,29 +1,40 @@
-a=int(input("Enter your age: "))
-if (a>18):
-    print("You are eligible to vote")
+# a=int(input("Enter your age: "))
+# if (a>18):
+#     print("You are eligible to vote")
+# else:
+#     print("You are not eligible for vote")
+
+# b=int (input("Enter any number : "))
+# if(b<0):
+#     print("num is negative")
+# elif(b==0):
+#     print("num is equal to zero")
+# else:
+#     print("num is positive")
+
+
+# # Nested 
+# age=int(input("Enter your age: "))
+# if(age==0):
+#     print("New Born")
+# elif(age>0):
+#     if(age<=10):
+#         print("Infant to toddler")
+#     elif(age>10 and age<20):
+#         print("You are a teenager")
+#     elif(age>20 and age<40):
+#         print("You are adult")
+
+# else:
+#     print("you are older")
+
+
+# Program to greet acc to time
+
+time=int(input("Enter time"))
+if(time>=0 and time<=12):
+    print("Good Morning")
+elif(time>=12 and time<=16):
+    print("Good Afternoon")
 else:
-    print("You are not eligible for vote")
-
-b=int (input("Enter any number : "))
-if(b<0):
-    print("num is negative")
-elif(b==0):
-    print("num is equal to zero")
-else:
-    print("num is positive")
-
-
-# Nested 
-age=int(input("Enter your age: "))
-if(age==0):
-    print("New Born")
-elif(age>0):
-    if(age<=10):
-        print("Infant to toddler")
-    elif(age>10 and age<20):
-        print("You are a teenager")
-    elif(age>20 and age<40):
-        print("You are adult")
-
-else:
-    print("you are older")
+    print("Good Evening")
